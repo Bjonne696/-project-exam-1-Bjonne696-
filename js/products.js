ada104f4e0f9bb418a965962d82f3a64f79efbf7
Bjorn_Tore <Bjonne696@gmail.com>
2022-09-30 17:14:15 +0200

    update

--- js/products.js
+++ js/products.js
@@ -1,21 +1,10 @@
-const baseUrl = "https://campspace.one//wp-json/wp/v2/posts"
-const postContainer = document.querySelector(".posts");
-
-async function getPosts(url){
-    const response = await fetch(url);
-    const posts = await response.json();
-    createHTML(posts)
-}
 
 
-getPosts(baseUrl);
 
-function createHTML(posts){
-    posts.forEach(function(post){
-        postContainer.innerHTML += `<div class="post">
-        <h2>${post.content}</h2>
-
-        </div>`;
-    })
-}
-       // <img src="${post.images[0].src}" alt="${post.name}"> //
+fetch("https://campspace.one//wp-json/wp/v2/posts").then((data)=>{
+    return data.json();
+}).then((completedata)=>{
+    console.log(completedata[0].title);
+    document.getElementById("root").
+    innerHTML=completedata[0].title;
+});
