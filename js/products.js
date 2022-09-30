04bb2e76c57532c016e895661b50af59a82c315a
Bjorn_Tore <Bjonne696@gmail.com>
2022-09-30 20:06:37 +0200

    update

--- js/products.js
+++ js/products.js
@@ -1,8 +1,22 @@
+const baseUrl = "https://campspace.one/wp-json/wp/v2/posts"
+const postContainer = document.querySelector(".posts");
+
+async function getPosts(url){
+    const response = await fetch(url);
+    const posts = await response.json();
+    createHTML(posts)
+}
 
 
+getPosts(baseUrl);
 
-fetch("https://campspace.one//wp-json/wp/v2/posts").then((data)=>{
-    return data.json();
-}).then((completedata)=>{
-    console.log(completedata.title);
-});
+function createHTML(posts){
+    posts.forEach(function(post){
+        postContainer.innerHTML += `<div class="post">
+        <h2>${post.title}</h2>
+
+        </div>`;
+        console.log(posts)
+    })
+}
+
