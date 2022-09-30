b38fcca9a181f87b172ac05dd30447eafc1f43b7
Bjorn_Tore <Bjonne696@gmail.com>
2022-09-30 19:44:01 +0200

    Update products.js

--- js/products.js
+++ js/products.js
@@ -4,7 +4,5 @@
 fetch("https://campspace.one//wp-json/wp/v2/posts").then((data)=>{
     return data.json();
 }).then((completedata)=>{
-    console.log(completedata[0].title);
-    document.getElementById("root").
-    innerHTML=completedata[0].title;
+    console.log(completedata.title);
 });
