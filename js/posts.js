8b10678180969298d85ce2ec9a7c80b67f4b188d
Bjorn_Tore <Bjonne696@gmail.com>
2022-10-01 18:57:05 +0200

    update

--- js/posts.js
+++ js/posts.js
@@ -1,4 +1,4 @@
-const baseUrl = "https://campspace.one/wp-json/wp/v2/posts?_embed"
+const baseUrl = "https://campspace.one/wp-json/wp/v2/posts?per_page=100"
 const postContainer = document.querySelector(".posts");
 
 getPosts(baseUrl);
@@ -6,25 +6,32 @@ getPosts(baseUrl);
 async function getPosts(url){
     const response = await fetch(url);
     const posts = await response.json();
-    //console.log(posts);
+    console.log(posts);
     posts.forEach(function(post){
     postContainer.innerHTML += createPost(post); 
     })
 }
 
 function createPost(post){
-   // var StrippedString = post.content.rendered.replace(/(<([^>]+)>)/ig,"");
-   // const decodedPost= JSON.parse(StrippedString);
-    console.log(post);
+    // var StrippedString = post.content.rendered.replace(/<p>*<\/p>/g, "");
+    // const decodedPost= JSON.parse(StrippedString);
+    // console.log(post);
     const createdPost=`
     <div class="card">
     <div class="card-title"><h2>${post.title.rendered}</h2>
-    <img "${post.content.rendered}"></img>
-
+    ${post.content.rendered}
     </div>    
     </div>
     `
-    // console.log(createdPost);
+  
+   
+     //console.log(StrippedString);
+     // console.log(createdPost);
     return createdPost;
+    const finalPost = document.querySelector('.p');
+    finalPost.style.display = none; 
+
 }
+
+
 
