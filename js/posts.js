8894bc5564ed351f552fc63516d541363dbfb39b
Bjorn_Tore <Bjonne696@gmail.com>
2022-10-01 23:08:17 +0200

    Update posts.js

--- js/posts.js
+++ js/posts.js
@@ -1,4 +1,4 @@
-const baseUrl = "https://campspace.one/wp-json/wp/v2/posts?per_page=100"
+const baseUrl = "https://campspace.one/wp-json/wp/v2/posts?per_page=100&_embed"
 const postContainer = document.querySelector(".posts");
 
 getPosts(baseUrl);
@@ -19,10 +19,18 @@ function createPost(post){
     const createdPost=`
     <div class="card">
     <div class="card-title"><h2>${post.title.rendered}</h2>
+    <img src="${post._embedded?.["wp:featuredmedia"][0].source_url}" alt="${post._embedded?.["wp:featuredmedia"][0].alt_text}" /<                 <h2>${post.title.rendered}</h2>
+              ${post.content.rendered}
+    
+   
+
     ${post.content.rendered}
+    
+    
     </div>    
     </div>
     `
+  
   
    
      //console.log(StrippedString);
