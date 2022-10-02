37f6a6aa477da198f44773875e12a9bdbc517942
Bjorn_Tore <Bjonne696@gmail.com>
2022-10-02 19:59:18 +0200

    update

--- js/script.js
+++ js/script.js
@@ -1,7 +1,41 @@
+
+
 const slidesContainer = document.getElementById("slides-container");
 const slide = document.querySelector(".slide");
-const prevButton = document.getElementById("slide-arrow-prev");
-const nextButton = document.getElementById("slide-arrow-next");
+const baseUrl = "https://campspace.one/wp-json/wp/v2/posts?per_page=100&_embed"
+const postContainer = document.querySelector(".slides");
+
+function createSlide(post,index) {
+  const slide = `
+  <div id="slides__${index+1}" class="slide">
+  <span class="slide__text">${post.content.rendered}</span>
+  <a class="slide__prev" href="#slides__${getPrevious(index+1)}" title="Previous"></a>
+  <a class="slide__next" href="#slides__${getNext(index-1)}" title="Next"></a>
+  </div>
+  `;
+  return slide;
+}
+
+function getPrevious(currentPost){
+  if(currentPost === 1){
+    return 4;
+  }
+  else{
+    return currentPost -1;
+  }
+ }
+
+ function getNext(currentPost){
+  if(currentPost === 4){
+    return 1;
+  }
+  else{
+    return currentPost +1;
+  }
+ }
+
+ getPosts(baseUrl);
+
 
 nextButton.addEventListener("click", () => {
   const slideWidth = slide.clientWidth;
@@ -12,3 +46,36 @@ prevButton.addEventListener("click", () => {
   const slideWidth = slide.clientWidth;
   slidesContainer.scrollLeft -= slideWidth;
 });
+
+
+
+
+
+
+async function getPosts(url){
+    try {
+    const response = await fetch(url);
+    const posts = await response.json();
+    console.log(posts);
+    document.querySelector('.loader').classList.add('hide');
+    for(let i = 0; i<4;i++){
+      postContainer.innerHTML += createSlide(posts[i],i);
+    }
+    
+
+    } catch (error) {
+        document.querySelector('.alert').innerHTML = alertUser(
+            'Error occured (Cannot load content)',
+            'error'
+            );
+
+    } finally {
+        setTimeout(function () {
+            document.querySelector('.alert').innerHTML = '';
+        }, 3000)
+
+    }
+
+    
+}
+
--- js/selected-posts.js
+++ js/selected-posts.js
@@ -3,35 +3,38 @@ const params = new URLSearchParams(queryString);
 const id = params.get('id');
 
 async function getPost(postId) {
+
+
   try {
-    console.log(postId);
+
     const response = await fetch(
-        'https://campspace.one/wp-json/wp/v2/posts' + postId_embed
+        'https://campspace.one/wp-json/wp/v2/posts/' + postId + "?_embed"
     );
     const jsonResults = await response.json();
-    const postArray = jsonResults.results;
-    console.log(postArray);
 
     document.querySelector('.loader').classList.add('hide');
 
-    document.title = postArray.title.rendered;
-    document.querySelector('h1').innerHTML = `${postArray.title.rendered}`;
+    document.title = jsonResults.title.rendered;
+    console.log(jsonResults);
+
+    document.querySelector('h1').innerHTML = `${jsonResults.title.rendered}`;
     document.querySelector(
       '.hero__img'
-    ).style.backgroundImage = `url('${postArray._embedded?.["wp:featuredmedia"][0].source_url}')`;
-    document.querySelector('.text').innerHTML = `${postArray.excerpt.rendered}`
+    ).style.backgroundImage = `url('${jsonResults._embedded?.["wp:featuredmedia"][0].source_url}')`;
+    document.querySelector('.text').innerHTML = `${jsonResults.excerpt.rendered}`
 
-
-} catch (error) {
+ 
+ } catch (error) {
     document.querySelector('.alert').innerHTML = alertUser(
         'Error occured (Cannot load content)',
         'error'
     );
-} finally {
+console.log(error);
+ } finally {
     setTimeout(function () {
         document.querySelector('.alert').innerHTML = '';
     }, 3000)
-}
+ }
 }
 
 getPost(id);
