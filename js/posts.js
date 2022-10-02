095e4b8024c60b532cf40738023b2f70fd41eadb
Bjorn_Tore <Bjonne696@gmail.com>
2022-10-02 11:25:46 +0200

    Update posts.js

--- js/posts.js
+++ js/posts.js
@@ -1,37 +1,27 @@
+const baseUrl = "https://campspace.one/wp-json/wp/v2/posts?per_page=100&_embed"
+const postContainer = document.querySelector(".posts");
 
+getPosts(baseUrl);
 
-
-
-const magicUrl = '';
-
-async function getMagic() {
-    try {
-        const response = await fetch('https://campspace.one/wp-json/wp/v2/posts?per_page=100&_embed');
-        const jsonFromServer = await response.json();
-        console.log(jsonFromServer.posts);
-        const magicResults = jsonFromServer.posts;
-
-        document.querySelector('.loader').classList.add('hide');
-
-        magicResults.forEach(function (value) {
-        document.querySelector('main').innerHTML += `
-            <div class="card-title">
-            <h2>${value.title}</h2>
-            <img src="${post._embedded?.["wp:featuredmedia"][0].source_url}" alt="${post._embedded?.["wp:featuredmedia"][0].alt_text}" />
-            <a class="button" href="selected-post.html?id=${post.id}"></a>
-        </div>`;
-        });
-    } catch (error) {
-        document.querySelector('.alert').innerHTML = alertUser(
-            'Error occured (Cannot load content)',
-            'error'
-        );
-    } finally {
-        setTimeout(function () {
-            document.querySelector('.alert').innerHTML = '';
-        }, 3000)
-    }
+async function getPosts(url){
+    const response = await fetch(url);
+    const posts = await response.json();
+    console.log(posts);
+    posts.forEach(function(post){
+    postContainer.innerHTML += createPost(post); 
+    })
 }
 
-getMagic(magicUrl);
+function createPost(post){
+    const createdPost=`
+    <div class="card">
+    <div class="card-title"><h2>${post.title.rendered}</h2>
+     <img src="${post._embedded?.["wp:featuredmedia"][0].source_url}" alt="${post._embedded?.["wp:featuredmedia"][0].alt_text}" />
 
+    <a href="selected-post.html?id=${post.id}"></a>
+    </div>    
+    </div>
+    `
+console.log(createdPost);
+    return createdPost;
+}
