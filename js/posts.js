abaa414faa5a65311db69393a1da20be9631410d
Bjorn_Tore <Bjonne696@gmail.com>
2022-10-02 22:41:40 +0200

    update

--- js/posts.js
+++ js/posts.js
@@ -1,38 +1,3 @@
-// const baseUrl = '';
-// getPost();
-// async function getPost() {
-//     try {
-//         const response = await fetch('https://campspace.one/wp-json/wp/v2/posts?per_page=100&_embed');
-//         const jsonFromServer = await response.json();
-//         console.log(jsonFromServer.posts);
-//         const postResults = jsonFromServer.posts;  !!!!!
-
-//         document.querySelector('.loader').classList.add('hide');
-
-//         postResults.forEach(function (value) {
-//         document.querySelector('main').innerHTML += `
-//             <div class="post-container">
-//             <div class="card-title"><h2>${value.title.rendered}</h2>
-//             <img src="${value._embedded?.["wp:featuredmedia"][0].source_url}" alt="${value._embedded?.["wp:featuredmedia"][0].alt_text}" />
-//             <a class="button" href="selected-post.html?id=${value.id}">Read More</a>
-//             </div>`;
-//         });
-//     } catch (error) {
-//         document.querySelector('.alert').innerHTML = alertUser(
-//             
-//             
-//         
-//     } finally {
-//         
-//             
-//         
-//     }
-// }
-
-
-
-
-
 
 const baseUrl = "https://campspace.one/wp-json/wp/v2/posts?per_page=100&_embed"
 const postContainer = document.querySelector(".posts");
